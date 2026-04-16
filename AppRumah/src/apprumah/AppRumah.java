8b1ba5be684d3592fb8b4bf7c658cd45056a72fa
Arojasa03 <arojasaarjahar@gmail.com>
2026-04-16 17:26:40 +0700

    Tugas Pertemuan 6
    Input data yang akan dicari pada aplikasi sebelumnya yang
    penjualan rumah (dapat berdasarkan Nama Pembeli atau
    Nama Area) tergantung dari data yang diinput.

---
+++ AppRumah/src/apprumah/AppRumah.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package apprumah;
+
+import apprumah.FormRumah;
+
+/**
+ *
+ * @author Arojasa Harefa
+ */
+public class AppRumah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FormRumah().setVisible(true);
+    }
+    
+}
